3e75507f803d33485a0cbd26bb986a0cfefdaa4a
kilionrotich <kilionkimurgor@gmail.com>
2026-06-20 14:56:43 +0300

    port server update

--- backend/src/server.js
+++ backend/src/server.js
@@ -52,7 +52,7 @@ app.get('/', (req, res) => {
 })();
 
 // Start server
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 10000; // fallback for local dev
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
